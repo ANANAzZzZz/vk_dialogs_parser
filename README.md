b0622c0aae775076095d5e28ef79b2b8da30953a
ANANAzZzZz <vladscooltrol@gmail.com>
2022-01-31 21:36:26 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### About
+
+Script for parcing your dialogs in social network VK
+on python
+
+### How to setup project
+
+1. Import project in Pycharm
+2. Download library bs4 0.0.1 version
+3. Download library emoji 1.6.4 version
+4. Download library lxml 4.7.1 version
+
+### How to use
+
+1. Download html data with vk dialogs
+2. Paste html data inside project
+3. Copy adress on html data from explorer and paste in console
+after starting script          ( example: vk_dialogues/messages/111111111 )
---
+++ parcer.py
@@ -0,0 +1,73 @@
+from bs4 import BeautifulSoup
+import emoji
+import os
+
+
+def get_text_dialogs():
+
+    print('Input link on directory with html data from vk dialogs: ')
+    print('(Directory should be inside project)')
+    eternal_link = input()
+
+    # get names of all html documents from directory
+    all_data = os.listdir(eternal_link)
+
+    number_of_html = 0
+    for element in all_data:
+        link = eternal_link + element
+        get_messages(link)
+
+        print('parsing html № ', number_of_html)
+        number_of_html += 1
+
+
+def get_messages(link):
+    data = open(link, "r")
+    html = data.read()
+
+    soup = BeautifulSoup(html, 'lxml')
+    items = soup.find_all('div', class_='item')
+
+    parce_html_items(items)
+
+
+def parce_html_items(items):
+    # open output file
+    output = open('dialogs.txt', 'a')
+
+    # counter for spliting dialogs on logic blocks
+    counter = 0
+
+    for item in reversed(items):
+        # searching tag message inside tag item
+        message = item.find('div', class_='message')
+
+        try:
+            # if message is attachment decompose it
+            item = item.find('div', class_='attachment').decompose()
+            message = None
+        except:
+            pass
+
+        if message is not None:
+            # if message is text start parcing
+            message.div.decompose()
+            message = message.get_text().strip()
+
+            try:
+                # check the message on emoji
+                if ":" not in emoji.demojize(message):
+                    # if it is not an emoji, write message to the output
+                    output.write(chr(10))
+                    output.write('- ' + message)
+
+                    # add logical indents
+                    if counter % 2 == 0:
+                        output.write(chr(10))
+                    counter += 1
+            except:
+                pass
+
+get_text_dialogs()
+print('Data parced succesfuly \n'
+      'check it out inside project directory')
